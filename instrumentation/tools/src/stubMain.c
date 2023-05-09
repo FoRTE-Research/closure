bc31bb0a5dcab32b63e5ec819d29379eb587496b
Rishi Ranjan <rranjan@cs.iitr.ac.in>
2023-05-09 11:50:38 -0400

    Add file hooks to closure stub

--- instrumentation/tools/src/stubMain.c
+++ instrumentation/tools/src/stubMain.c
@@ -151,39 +151,69 @@ void restore_global_sections(char *closure_global_section_addr, char *closure_gl
     return;
 }
 
-FILE * open_handles[40];
-int num_open_handles = 0;
+typedef struct
+{
+    FILE *f;                                               /* we'll use this field as the key */
+    int i;                                                 /* this field is for the hashed object */
+    UT_hash_handle hh; /* makes this structure hashable */ // IDK how this works, they just said it does and it did
+} file_ptr_obj;
 
-FILE *fopen_hook(const char *pathname, const char *mode) {
-    FILE * f = fopen(pathname, mode);
-    printf("Fopen called for %s return %p\n", pathname, f);
+file_ptr_obj *file_ptr_map = NULL;
 
-    open_handles[num_open_handles] = f;
-    num_open_handles++;
+void add_file_ptr(FILE *key_ptr)
+{
+    file_ptr_obj *s;
+
+    s = malloc(sizeof(file_ptr_obj));
+    s->f = key_ptr;
+    s->i = 1;
+    HASH_ADD_PTR(file_ptr_map, f, s); /* id: name of key field!!! LOOK MORE INTO THIS, REPLACE WITH THE CORRECT
+                                      FUNCTION FOR ADDING A VOID POINTER AND THEN RECREATE WITH FIND */
+}
+
+// Responsible for finding the pointer to confirm that
+file_ptr_obj *find_file_ptr(FILE *ptr_id)
+{
+    file_ptr_obj *s;
+
+    HASH_FIND_PTR(file_ptr_map, &ptr_id,
+                  s); // Change to the correct function for storing a void pointer, maybe this can be simplified.
+    return s;
+}
+
+// Deletes the pointer object from the hash table
+void delete_file_ptr(file_ptr_obj *obj)
+{
+    if (obj != NULL)
+    {
+        HASH_DEL(file_ptr_map, obj);
+        free(obj);
+    }
+}
+
+FILE *fopen_hook(const char *pathname, const char *mode)
+{
+    FILE *f = fopen(pathname, mode);
+    add_file_ptr(f);
     return f;
 }
 
-int fclose_hook(FILE *f) {
-    for (int i =0; i < num_open_handles; ++i) {
-        if (open_handles[i] == f) {
-            open_handles[i] = NULL;
-        }
-    }
-    printf("Fclose called for %p\n", f);
-    return fclose(f);
+int fclose_hook(FILE *f)
+{
+    file_ptr_obj *p = find_file_ptr(f);
+    int ret = fclose(f);
+    delete_file_ptr(p);
+    return ret;
 }
 
-void close_open_file_handles() {
-
-    for (int i = 0; i < 20; ++i) {
-        if (open_handles[i] != NULL) {
-            printf("Closing %p\n", open_handles[i]);
-            fclose(open_handles[i]);
-        }
-        open_handles[i] = NULL;
+void close_open_file_handles()
+{
+    file_ptr_obj *s, *tmp;
+    HASH_ITER(hh, file_ptr_map, s, tmp)
+    {
+        fclose(s->f);
+        delete_file_ptr(s);
     }
-    num_open_handles = 0;
-
 }
 
 // Might want to account for frees that happen out of order,
@@ -215,8 +245,8 @@ int main(int argc, char *argv[])
             ret = start_main(argc, argv);
         }
 
-        free_ptrs();
         close_open_file_handles();
+        free_ptrs();
         printf("Closure - section addr %p\n", closure_global_section_addr);
         restore_global_sections(closure_global_section_addr, closure_global_section_copy, closure_global_section_size);
     }
