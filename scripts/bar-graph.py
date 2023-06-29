7c8ddf444827b752414920fb4315d4bb96d502df
Rishi Ranjan <rishiranjan@vt.edu>
2023-06-29 08:41:23 -0700

    Update graphing scripts

--- scripts/bar-graph.py
+++ scripts/bar-graph.py
@@ -2,18 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 benchmarks = ["bsdtar", "nasm", "cJSON", "xmllint", "freetype", "pngfix",
-              "json-parser", "giftext", "gpmf-parser", "libpcap", "zlib", "c-blosc2", "average"]
+              "json-parser", "giftext", "gpmf-parser", "libpcap", "zlib", "c-blosc2",
+              "cgif", "libbpf", "libdwarf", "lighttpd", "oniguruma", "md4c"]
 
-afl_data_exec = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+afl_data_exec = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 closure_data_exec = [3.5, 1.06, 3.58, 2.18, 2.72,
-                     3.81, 2.71, 3.54, 2.13, 2.58, 3.57, 3.01, 2.865]
+                     3.81, 2.71, 3.54, 2.13, 2.58, 3.57, 3.01,
+                     1.71, 3.65, 2.64, 3.33, 3.06, 1.92]
 
 closure_data_cov = [1.52, 1.011, 1, 1.03, 1.038, 1.008,
-                    1.004, 1.0132, 1.143, 1.553, 1.013, 0.99, 1.1102]
+                    1.004, 1.0132, 1.143, 1.553, 1.013, 0.99, 1.002, 1.01, 1.02, 1.0041, 1.0054, 1.0004]
 
 
 closure_data_corpus = [1.661495367, 1.04, 1.06, 0.955,
-                       1.08, 1.05, 1.06, 0.56, 1.11, 1.45, 1.11, 1.032, 1.097]
+                       1.08, 1.05, 1.06, 0.56, 1.11, 1.45, 1.11, 1.032,
+                       1.009, 1.06, 1.09, 0.98, 1.03, 1.011]
+
+
+sum = 0
+
+for i in closure_data_exec:
+    sum += i
+
+print(f"Average exec speedup {sum/len(benchmarks)}")
+closure_data_exec.append(sum/len(benchmarks))
+
+sum = 0
+for i in closure_data_cov:
+    sum += i
+print(f"Average cov increase {sum/len(benchmarks)}")
+closure_data_cov.append(sum/len(benchmarks))
+
+sum = 0
+for i in closure_data_corpus:
+    sum += i
+print(f"Average corpus increase {sum/len(benchmarks)}")
+closure_data_corpus.append(sum/len(benchmarks))
+
+afl_data_exec.append(1)
+benchmarks.append("average")
 
 X_axis = np.arange(len(benchmarks))
 
--- scripts/scatter.py
+++ scripts/scatter.py
@@ -1,24 +1,28 @@
 import matplotlib.pyplot as plt
 
-coverage_improvement = [52.17, 1.18, 0, 3.08, 3.8, 0.83, 0.43, 1.32, 14.39, 55.32, 1.34, -0.47]
-exec_ratio = [3.5, 1.06, 3.58, 2.18, 2.72, 3.81, 2.71, 3.54, 2.13, 2.58, 3.57, 3.01]
+coverage_improvement = [10, 1.18, 0, 3.08, 3.8,
+                        0.83, 0.43, 1.31, 10, 10, 1.34, -0.47, 0.29, 1.9, 2, 0.41, 0.54, 0.04]
+exec_ratio = [3.5, 1.06, 3.58, 2.18, 2.72,
+              3.81, 2.71, 3.54, 2.13, 2.58, 3.6, 3.01, 1.71, 3.65, 2.64, 3.33, 3.06, 1.92]
 
-benchmarks = ["bsdtar", "nasm", "cJSON", "xmllint", "freetype", "pngfix", "json-parser", "giftext", "gpmf-parser", "libpcap", "zlib", "c-blosc2"]
+benchmarks = ["bsdtar", "nasm", "cJSON", "xmllint", "freetype", "pngfix",
+              "json-parser", "giftext", "gpmf-parser", "libpcap", "zlib", "c-blosc2", "cgif", "libbpf", "libdwarf", "lighttpd", "oniguruma", "md4c"]
 
 plt.scatter(coverage_improvement, exec_ratio)
 
 plt.xlabel("Coverage Improvement (% increase)")
 plt.ylabel("Exec speedup")
 
-plt.xticks([0, 5, 10, 20, 30, 40, 50])
+plt.xticks(ticks=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], labels=[
+           "0%", "1%", "2%", "3%", "4%", "5%", "6%", "7%", "8%", "9%", ">10%"])
 
-plt.plot([0,0], [0, 4], linestyle="--", color="C1")
+plt.plot([0, 0], [0, 4], linestyle="--", color="C1")
 
 for x, y, label in zip(coverage_improvement, exec_ratio, benchmarks):
-    plt.annotate(label, # this is the text
-                 (x,y), # these are the coordinates to position the label
-                 textcoords="offset points", # how to position the text
-                 xytext=(0,10), # distance from text to points (x,y)
+    plt.annotate(label,  # this is the text
+                 (x, y),  # these are the coordinates to position the label
+                 textcoords="offset points",  # how to position the text
+                 xytext=(0, 10),  # distance from text to points (x,y)
                  ha='center')
 
 plt.show()
